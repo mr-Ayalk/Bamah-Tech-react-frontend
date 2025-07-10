24f38c0d4d5b66c5b1840d925ec9ac4a0d3bf4d3
mr-Ayalk <ayalkteketel7@gmail.com>
2025-07-10 11:53:18 +0300

    base url in config updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  base:"/Bamah-Tech-react-frontend/"
-})
+  plugins: [tailwindcss(), react()],
+  base: "/",
+});
